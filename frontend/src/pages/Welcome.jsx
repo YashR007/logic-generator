11efd7833018c046b7e85e6a0bbdad90700b60d3
Yash Uchagavi <161680254+YashR007@users.noreply.github.com>
2026-04-07 21:43:51 +0530

    Create Welcome component with welcome message and animation

---
+++ frontend/src/pages/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Welcome.css';
+
+const Welcome = () => {
+  return (
+    <div className='welcome-container'>
+      <h1>Welcome to the Logic Generator!</h1>
+      <p>Made with ♡ Di x Dee's group</p>
+    </div>
+  );
+};
+
+export default Welcome;
