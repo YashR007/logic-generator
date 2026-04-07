1b0c301df54801347826d0a0d70cc6cc2dd78b06
Yash Uchagavi <161680254+YashR007@users.noreply.github.com>
2026-04-07 21:42:21 +0530

    Add Tailwind CSS configuration

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        white: '#ffffff',
+        'off-white': '#f8f8f8',
+        blue: {
+          light: '#3b82f6',
+          DEFAULT: '#1e40af',
+          dark: '#1e3a8a',
+        },
+        pink: {
+          light: '#fbcfe8',
+          DEFAULT: '#ec4899',
+          dark: '#db2777',
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
